305796bbc0489967a0599e4414dec02285660c11
Muhamad Adi Prasetyo <88260025+xTaMaox@users.noreply.github.com>
2023-01-06 17:32:11 +0700

    Add files via upload

---
+++ The Dining Philosophers.cpp
@@ -0,0 +1,37 @@
+#include <semaphore.h>
+class DiningPhilosophers {
+public:
+    vector<sem_t>v;
+    DiningPhilosophers() {
+        v.resize(5);
+        for(int i=0;i<5;i++) sem_init(&v[i],0,1);  
+    }
+    ~DiningPhilosophers(){
+        for(int i=0;i<5;i++) sem_destroy(&v[i]);
+    }
+	//to avoid deadlock, every odd philosopher picks up the left fork first, and every even philosopher picks up the right fork first.
+    void wantsToEat(int philosopher,
+                    function<void()> pickLeftFork,
+                    function<void()> pickRightFork,
+                    function<void()> eat,
+                    function<void()> putLeftFork,
+                    function<void()> putRightFork) {
+        int left=philosopher;
+        int right=(philosopher+1)%5;
+        if(philosopher&1){
+            sem_wait(&v[left]);
+            sem_wait(&v[right]);
+        }
+        else{
+            sem_wait(&v[right]);
+            sem_wait(&v[left]);
+        }
+        pickLeftFork();
+        pickRightFork();
+        eat();
+        putLeftFork();
+        putRightFork();
+        sem_post(&v[left]);
+        sem_post(&v[right]);
+    }
+};
